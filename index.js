6b02534d064c17aa5da76792275a51f8d1343242
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2017-11-15 00:18:14 +0900

    Add list

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ document.addEventListener("DOMContentLoaded", function() {
     __name: "sample.VueController",
     _data: {
       message: "hello world",
-      count: 0
+      count: 0,
+      list: []
     },
     __construct() {
       this._component = new Vue({
         el: this.$find("#vueRoot").get(0),
-        template: "#hello",
-        data: this._data
+        template: `
+        <div>
+        <input type="button" id="hello" value="add" />
+        {{message}}{{length}}
+          <ul>
+            <li v-for="item of list">{{item}}</li>
+          </ul>
+        </div>
+        `,
+        data: this._data,
+        computed: {
+          length () {
+            return this.list.length
+          }
+        }
       });
     },
     __ready() {},
     "#hello click": function() {
       this._data.count++;
+      this._data.list.push(`hello world ${this._data.count}`);
     }
   });
 });
